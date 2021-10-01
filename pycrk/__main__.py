f4eed4990ca5431b8057fdd212e2aa41bd2a9fdb
Carey Metcalfe <carey@cmetcalfe.ca>
2021-10-01 00:43:39 -0400

    Add a command to generate CRK files

--- pycrk/__main__.py
+++ pycrk/__main__.py
@@ -1,16 +1,63 @@
+#!/usr/bin/env python
+
 import argparse
 import logging
 import os
 from typing import Optional
 
-from pycrk import Crk
+from pycrk import make_file_crk, make_dir_crk, Crk
 
 
-def parse_args():
-    parser = argparse.ArgumentParser(description="Applies patches from CRK files")
-    parser.add_argument("file", help="The CRK file to use")
+def generate_crk() -> None:
+    """Generate a CRK file using two sets of file(s) and print it"""
+    logging.basicConfig(format="[%(levelname)8s] %(message)s", level=logging.WARNING)
+
+    parser = argparse.ArgumentParser(
+        description="Generate a CRK patch from 2 files/directories"
+    )
     parser.add_argument(
-        "--wd", help="The directory that holds the file(s) to patch", default="."
+        "original",
+        help="The original file/directory"
+    )
+    parser.add_argument(
+        "patched",
+        help="The patched file/directory"
+    )
+    parser.add_argument(
+        "-o", "--output",
+        metavar="<path>",
+        help="Write generated CRK patch to <path> instead of stdout",
+        type=argparse.FileType('w', encoding='utf-8'),
+        default="-"
+    )
+
+    args = parser.parse_args()
+
+    original_file, patched_file = (os.path.isfile(x) for x in (args.original, args.patched))
+    if original_file ^ patched_file:
+        raise ValueError("The two inputs need to both be files or directories")
+
+    if original_file:
+        crk = make_file_crk(args.original, args.patched)
+    else:
+        crk = make_dir_crk(args.original, args.patched)
+
+    print(crk.serialize(), file=args.output)
+
+
+def apply_crk():
+    logging.basicConfig(format="[%(levelname)8s] %(message)s", level=logging.WARNING)
+
+    parser = argparse.ArgumentParser(description="Apply a CRK patch")
+    parser.add_argument(
+        "file",
+        type=argparse.FileType('rt'),
+        help="The CRK file to apply (use '-' for stdin)"
+    )
+    parser.add_argument(
+        "--wd",
+        help="The directory that contains the file(s) to patch (default: %(default)s)",
+        default="."
     )
     parser.add_argument(
         "--ask",
@@ -32,18 +79,9 @@ def parse_args():
         default=None,
     )
 
-    return vars(parser.parse_args())
+    args = parser.parse_args()
 
-
-def process_crk(
-    file: str, wd: str = ".", ask: bool = False, apply: Optional[bool] = None
-) -> None:
-    """Process the CRK file
-
-    ask: if true, will ask before (un)applying patches
-    apply: False=unpatch, None=show, True=patch
-    """
-    crk = Crk.from_file(file)
+    crk = Crk.from_file(args.file)
 
     filename_size = 0
     for patch in crk:
@@ -60,29 +98,24 @@ def process_crk(
     print(crk.title)
     print()
     for patch in crk:
-        path = os.path.join(wd, patch.filename)
+        path = os.path.join(args.wd, patch.filename)
         if not os.path.isfile(path):
             msg("NO FILE", patch)
             continue
         with open(path, "r+b") as f:
             if not patch.valid(f):
                 msg("INVALID", patch)
-            elif (patched := patch.applied(f)) == apply or apply is None:
+            elif (patched := patch.applied(f)) == args.apply or args.apply is None:
                 msg(actions[patched], patch)
             elif (
-                not ask
-                or input(f"{questions[apply]} '{patch.title}'? [y/N]: ").lower() == "y"
+                not args.ask
+                or input(f"{questions[args.apply]} '{patch.title}'? [y/N]: ").lower() == "y"
             ):
-                patch.apply(f, unpatch=not apply)
-                msg(actions[apply], patch)
+                patch.apply(f, unpatch=not args.apply)
+                msg(actions[args.apply], patch)
             else:
                 msg("SKIPPED", patch)
 
 
-def main():
-    logging.basicConfig(format="[%(levelname)8s] %(message)s", level=logging.ERROR)
-    process_crk(**parse_args())
-
-
 if __name__ == "__main__":
-    main()
+    apply_crk()
